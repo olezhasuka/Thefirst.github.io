c9bdac33a4a4911a26dc624fc6865427dce7c1f8
olezhasuka <olezhka.churin@gmail.com>
2016-08-04 18:08:25 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.about_img').slick({
+	  	infinite: true,
+	  	slidesToShow: 4,
+	  	slidesToScroll: 1,
+	  	prevArrow: '<button type="button" class="slick-prev"></button>',
+	  	nextArrow: '<button type="button" class="slick-next"></button>'
+	});
+
+});
